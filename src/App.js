7d2325ca9a22bde610a04ac51c7f22174d7a32ac
jesusquezada <[]>
2022-04-19 15:14:24 -0700

    Feat(App): Added employees data axios call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+let [ employees, setEmployees ] = useState([]);
+  useEffect(() => {
+    async function getEmployees() {
+      const results = await axios('https://randomuser.me/api/');
+      setEmployees(results.data)
+    }
+    getEmployees();
+  },[])
+
+console.log(employees.results)
+
   return (
     <div className="App">
       Hello World 
