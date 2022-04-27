bd5cbda4ca04c9c3cdeb86f501362a15e45bd282
jesusquezada <[]>
2022-04-27 10:21:05 -0700

    feat(EmployeesTable): added isClicked state to EmployeesTable

--- src/components/EmployeesTable.js
+++ src/components/EmployeesTable.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 const EmployeesTable = (props) => {
+
+  const [isClicked, setIsClicked] = useState(false);
  
   const table = props.employees.map((obj) => {
     return {
