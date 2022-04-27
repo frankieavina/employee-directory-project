1b0776aba5d87d5c99966cda8d6eaea2a3f0420d
jesusquezada <[]>
2022-04-27 09:20:10 -0700

    feat(EmployeesTable): added filterByFirstName and filterByLastName onClick

--- src/components/EmployeesTable.js
+++ src/components/EmployeesTable.js
@@ -1,75 +1,62 @@
-import React, { useContext, useEffect, useState } from 'react';
-// importing context
-import EmployeeContext from '../context/EmployeeContext';
+import React, { useEffect, useState } from 'react';
 
-const EmployeesTable = () => {
+const EmployeesTable = (props) => {
+  // # JESUS/FRANKIE: To avoid weird async issues w/parent state generating child state, I just took the props.employee and stored them as a regular old JS variable...
 
-  // consuming or using employees context 
-  const { employees } = useContext(EmployeeContext); 
-    
-  const tableData = employees.map(obj => {
-      return {
-        "firstName": obj.name.first ,
-        "lastName": obj.name.last,
-        "email": obj.email,
-        "location": `${obj.location.city}, ${obj.location.state}`,
-        "phone": obj.phone
-      }
+  // # I also moved the filterByFirstName method to the App component and removed it from this file. This file now uses the version of filterByFirstName passed in as a prop from App.js
+  const table = props.employees.map((obj) => {
+    return {
+      firstName: obj.name.first,
+      lastName: obj.name.last,
+      email: obj.email,
+      location: `${obj.location.city}, ${obj.location.state}`,
+      phone: obj.phone,
+    };
   });
 
-
-
-// Filter First Name
-const filterByFirstName = () => {
-  alert('Clicked');
-}
-// Filter Last Name
-// Filter First Name
-// Filter First Name
-
-console.log('Test:', tableData);
-let names = tableData.map( obj => {
-  return obj.firstName;
-} )
-
-names = names.sort()
-console.log(names);
   return (
-    
     <div>
-    <table  style={{width: "920px", margin: "auto"}} className=" table text-success">
-    <thead>
-      <tr>
-        <th scope="col" onClick={ filterByFirstName }>
-          First Name 
-          </th>
-        <th scope="col">Last Name</th>
-        <th scope="col">Email Address</th>
-        <th scope="col">Department</th>
-        <th scope="col">Location</th>
-      </tr>
-    </thead>
-    <tbody>
-    
-     { 
-      tableData.map( obj => {
-       
-        return <tr>
-                  <td>{obj.firstName}</td>
-                  <td>{obj.lastName}</td>
-                  <td>{obj.email}</td>
-                  <td>{obj.location}</td>
-                  <td>{obj.phone}</td>
-                </tr>
-  
-    })  }
-  
-    
-    
-    </tbody>
-  </table>
-  </div>
-  )
-}
+      <table
+        style={{ width: '920px', margin: 'auto' }}
+        className=" table text-success"
+      >
+        <thead>
+          <tr>
+            <th
+              scope="col"
+              onClick={() => {
+                props.filterByFirstName();
+              }}
+            >
+              First Name
+            </th>
+            <th scope="col"
+             onClick={() => {
+              props.filterByLastName();
+            }}>
+              Last Name
+            </th>
+            <th scope="col">Email Address</th>
+            <th scope="col">Department</th>
+            <th scope="col">Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table.map((obj) => {
+            return (
+              <tr>
+                <td>{obj.firstName}</td>
+                <td>{obj.lastName}</td>
+                <td>{obj.email}</td>
+                <td>{obj.location}</td>
+                <td>{obj.phone}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default EmployeesTable;
